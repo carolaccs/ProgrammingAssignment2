9fe3a0f0fd78b5363b5a6b3a2d7f368eca201ea2
I.C. Martinez <carolaccs@gmail.com>
2015-11-14 13:17:43 -0430

    Adding usage example
    Completing comments and adding an usage example

--- cachematrix.R
+++ cachematrix.R
@@ -3,21 +3,20 @@
 # in R course.
 #
 # The first function makeCacheMatrix creates a special matrix, that can cache its inverse.
-#  
 # The second function cacheSolve calculates a matrix (created with makeCacheMatrix) inverse 
 # only if this inverse have not been previouly calculated. 
 #
 # Ivette C. Martinez
 #
 
-
+#
 # makeCacheMatrix creates a special "matrix" object that can cache its inverse.
 #
 # This special "matrix" is a list that contains functions to:
-# 1. Set the values of the matrix "m"
-# 2. Get the values of the matrix "m"
-# 3. Set the values of the matrix
-# 4. Get the values of the matrix
+# 1. Set the values of the matrix "x"
+# 2. Get the values of the matrix "x"
+# 3. Set the values of the inverse matrix of "x"
+# 4. Get the values of the inverse matrix of "x"
 #
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -41,12 +40,24 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         mInv <- x$getInv()
-        if(!is.null(mInv)){
+        if(!is.null(mInv)){  ## The inverse have been prevously calculated
         	message("getting cached inverse")
         	return(mInv)
         }
+        ## Calculating the inverse of x
         data <- x$get()
         mInv <-solve(data, ...)
         x$setInv(mInv)
         mInv 
 }
+
+#
+# An example of usage:
+# > A <- matrix(c(1,2,2,1), nrow=2, byrow=TRUE)
+# > print(A)
+# > source("cachematrix.R")
+# > M <- makeCacheMatrix(A)
+# > I <- cacheSolve(M)
+# > J <- cacheSolve(M)
+# getting cached inverse
+# 
